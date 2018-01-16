28210c2067b2a4e9771624c2fbb8024bdfe148f8
LaKeshia Johnson <lakeshiajohnson@LaKeshias-MacBook-Pro.local>
2018-01-15 18:55:56 -0600

    functionality working

--- scripts.js
+++ scripts.js
@@ -1,9 +1,26 @@
 console.log("scripts.js is loading");
-//listen for the key down. when we hear the key down,
-//run the function thst will give us the event
 
-window.addEventListener("keydown", function (e) {
-	//console.log("EVENT: ", e);
-	console.log("EVENT: ", e.keycode);
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+  }
 
-});
+  function playSound(e) {
+  	//console.log("EVENT: ", e);
+	//console.log("EVENT: ", e.keycode);
+	//select audo file with data key that matches the pressed key code
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return; //stops the function from running all together
+
+    key.classList.add('playing'); //adding css to animate keys
+    audio.currentTime = 0; //rewinds sound to the beginning
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  
+//listen for the key down. when we hear the key down
+//run the function playSound
+  window.addEventListener('keydown', playSound);
